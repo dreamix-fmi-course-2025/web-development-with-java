f28bda12daec4b9180fd8118997d67b6d68148fc
GeorgiMinkov <georgi.minkov@dreamix.eu>
2025-04-01 22:23:39 +0300

    Week06 Examples

--- lab-project/inventorysystem/src/main/java/bg/fmi/uni/inventorysystem/controller/InventoryItemController.java
+++ lab-project/inventorysystem/src/main/java/bg/fmi/uni/inventorysystem/controller/InventoryItemController.java
@@ -5,12 +5,16 @@ import bg.fmi.uni.inventorysystem.config.logger.Logger;
 import bg.fmi.uni.inventorysystem.dto.InventoryItemDto;
 import bg.fmi.uni.inventorysystem.model.InventoryItem;
 import bg.fmi.uni.inventorysystem.service.InventoryItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Controller for inventory operations.
@@ -48,6 +52,13 @@ public class InventoryItemController {
         } else {
             items.forEach(item -> logger.info("Item: " + item.getName() + ", Quantity: " + item.getQuantity()));
         }
+    }
+
+    @GetMapping("items/{id}")
+    public ResponseEntity<InventoryItemDto> getItemById(@PathVariable int id) {
+        Optional<InventoryItem> item = inventoryItemService.getItemById(id);
+        return item.map(inventoryItem -> ResponseEntity.ok(InventoryItemDto.fromEntity(inventoryItem)))
+                .orElseGet(() -> new ResponseEntity<> (HttpStatus.NOT_FOUND));
     }
 
     public List<InventoryItem> getLowStockItems() {
